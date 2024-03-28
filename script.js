14583565f9c5a0fc322b4b43cee38a6570176b1a
Luksh Mahajan <113115939+LukshMahajan@users.noreply.github.com>
2024-03-28 10:48:43 -0400

    new

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function subpage1(){
     window.location.href="subpage1";
 }
+function subpage2(){
+    window.location.href="subpage2";
+}
+function subpage3(){
+    window.location.href="subpage3";
+}
